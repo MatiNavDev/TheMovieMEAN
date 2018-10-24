592a54db1833399bee7461bd3582331e73950e50
MatiNavDev <matiasnavarrodev@gmail.com>
2018-10-24 18:20:27 -0300

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,22 @@ if (environment.production) {
 }
 
 
-  platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('ngsw-worker.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      //registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+this.ngZone.runOutsideAngular(() => {
+  this.slideIntervalId = setInterval(() => {
+    this.ngZone.run(() => {
+      platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
+        if ('serviceWorker' in navigator) {
+          navigator.serviceWorker.register('ngsw-worker.js').then(function (registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          }).catch(function (err) {
+            //registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+          });
+        } else {
+          console.log('No service-worker on this browser');
+        }
+      }).catch(err => console.log(err));
+      this.cd.markForCheck();
     });
-  }else {
-    console.log('No service-worker on this browser');
-  }
-  }).catch(err => console.log(err));
+  }, 5000);
+});
