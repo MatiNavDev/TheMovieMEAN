b4d4ca3a042070ccb325a8d4f1bf0893b35811ec
MatiNavDev <matiasnavarrodev@gmail.com>
2018-10-24 22:51:59 -0300

    fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ApplicationRef } from '@angular/core';
 import { SessionService } from './common/services/session.service';
 
 @Component({
@@ -6,14 +6,30 @@ import { SessionService } from './common/services/session.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   constructor(
-    private sessionSrvc:SessionService
-    ){}
+    private sessionSrvc: SessionService, private applicationRef: ApplicationRef
+  ) {
+    this.applicationRef.isStable.subscribe(isStable => {
+      if (isStable) {
+        if ('serviceWorker' in navigator) {
+          navigator.serviceWorker.register('ngsw-worker.js').then(function (registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          }).catch(function (err) {
+            //registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+          });
+        } else {
+          console.log('No service-worker on this browser');
+        }
+      }
+    })
+  }
 
-    ngOnInit(){
-      this.sessionSrvc.isAuthenticated();
-    }
-  
+  ngOnInit() {
+    this.sessionSrvc.isAuthenticated();
+
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -8,23 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-const self = this;
-this.ngZone.runOutsideAngular(() => {
-  this.slideIntervalId = setInterval(() => {
-    self.ngZone.run(() => {
-      platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
-        if ('serviceWorker' in navigator) {
-          navigator.serviceWorker.register('ngsw-worker.js').then(function (registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-          }).catch(function (err) {
-            //registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-          });
-        } else {
-          console.log('No service-worker on this browser');
-        }
-      }).catch(err => console.log(err));
-      this.cd.markForCheck();
-    });
-  }, 5000);
-});
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
