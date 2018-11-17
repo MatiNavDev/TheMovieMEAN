4b38e1a7980bb09dc81707e7ed80beaa67a4d136
MatiNavDev <matiasnavarrodev@gmail.com>
2018-11-16 23:17:56 -0300

    eliminado de TODO

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,7 @@
-if (process.env.NODE_ENV === 'production') { // pregunta si esta en modo produccion o en modo desarrollo
+if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else if (process.env.NODE_ENV === 'test') {
   module.exports = require('./test');
 } else {
   module.exports = require('./dev');
 }
-
-
-// TODO: Cuando se sube a prod borrar el dev y dejar solo el produ,
-// donde voy a poner las variables de entorno
